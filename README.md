733bdf2c9450bd1954313862311e1cc9374b6280
Eduardo Mioto <eduardo.mioto@ge.com>
2016-11-11 20:08:08 -0200

    Inclusão de novos sons e hot heys

--- README.md
+++ README.md
@@ -1,27 +1,29 @@
 # butoneira
 ###### Documentação:
 	
-- [FILA 1, POSIÇÃO 1] Pelé 		(ALT + P)		
-- [FILA 1, POSIÇÃO 2] Hadouken 	(ALT + H)
-- [FILA 1, POSIÇÃO 3] Grilo		(ALT + G)
-- [FILA 1, POSIÇÃO 4] Arquivo X	(ALT + X)     	
-	
-- [FILA 2, POSIÇÃO 1] Chaves Triste  		(ALT + T)		
-- [FILA 2, POSIÇÃO 2] Kiko Bola  		  	(ALT + K)		
-- [FILA 2, POSIÇÃO 3] Moises To Entendendo 	(ALT + M)		
-- [FILA 2, POSIÇÃO 4] Flauta				(ALT + F)
-	
-- [FILA 3, POSIÇÃO 1] Joao Kleber				(ALT + J)
-- [FILA 3, POSIÇÃO 2] Matarra 1000 				(ALT + 1)
-- [FILA 3, POSIÇÃO 3] Ta vazii 					(ALT + 0)
-- [FILA 3, POSIÇÃO 4] Hoje Não					(ALT + N)	
+- [FILA 1, POSIÇÃO 1] Pelé 					(ALT + P)		
+- [FILA 1, POSIÇÃO 2] Hadouken 				(ALT + H)
+- [FILA 1, POSIÇÃO 3] Grilo					(ALT + G)
+- [FILA 1, POSIÇÃO 4] Arquivo X				(ALT + X)     	
+- [FILA 1, POSIÇÃO 5] Baú da Felicidade		(ALT + B)		
+- [FILA 1, POSIÇÃO 6] Bateria	  		  	(ALT + 9)		
 
-- [FILA 4, POSIÇÃO 1] Tetra						(ALT + 4)
-- [FILA 4, POSIÇÃO 2] ?????						(ALT + ?)
-- [FILA 4, POSIÇÃO 3] ????? 					(ALT + ?)
-- [FILA 4, POSIÇÃO 4] ?????						(ALT + ?)
+- [FILA 2, POSIÇÃO 1] A Praça é Nossa	 	(ALT + N)		
+- [FILA 2, POSIÇÃO 2] Risada Chaves			(ALT + R)
+- [FILA 2, POSIÇÃO 3] Joao Kleber			(ALT + J)
+- [FILA 2, POSIÇÃO 4] Matarra 1000 			(ALT + 1)
+- [FILA 2, POSIÇÃO 5] Ta vazii 				(ALT + 8)
+- [FILA 2, POSIÇÃO 6] Hoje Não				(ALT + 3)	
+
+- [FILA 4, POSIÇÃO 1] Tetra					(ALT + 4)
+- [FILA 4, POSIÇÃO 2] Finish Him			(ALT + Z)
+- [FILA 4, POSIÇÃO 3] R2D2	 				(ALT + 2)
+- [FILA 4, POSIÇÃO 4] Ai que burro			(ALT + 0)
+- [FILA 4, POSIÇÃO 5] Erro XP				(ALT + E)
+- [FILA 4, POSIÇÃO 6] Metal Gear			(ALT + M)
+
 ##### Sugestões:
- Mestre dos Magos
+- Mestre dos Magos
 - Robot
 - Palmas
 
--- custom/butoneira.js
+++ custom/butoneira.js
@@ -25,21 +25,22 @@
 		jQuery(document).bind('keydown', 'alt+h',function (evt){playSound("#audio2"); return false; });
 		jQuery(document).bind('keydown', 'alt+g',function (evt){playSound("#audio3"); return false; });
 		jQuery(document).bind('keydown', 'alt+x',function (evt){playSound("#audio4"); return false; });
+		jQuery(document).bind('keydown', 'alt+b',function (evt){playSound("#audio5"); return false; });
+		jQuery(document).bind('keydown', 'alt+9',function (evt){playSound("#audio6"); return false; });
 		
-		jQuery(document).bind('keydown', 'alt+t',function (evt){playSound("#audio5"); return false; });
-		jQuery(document).bind('keydown', 'alt+k',function (evt){playSound("#audio6"); return false; });
-		jQuery(document).bind('keydown', 'alt+m',function (evt){playSound("#audio7"); return false; });
-		jQuery(document).bind('keydown', 'alt+f',function (evt){playSound("#audio8"); return false; });
-		
+		jQuery(document).bind('keydown', 'alt+n',function (evt){playSound("#audio7"); return false; });
+		jQuery(document).bind('keydown', 'alt+r',function (evt){playSound("#audio8"); return false; });
 		jQuery(document).bind('keydown', 'alt+j',function (evt){playSound("#audio9"); return false; });
 		jQuery(document).bind('keydown', 'alt+1',function (evt){playSound("#audio10"); return false; });
-		jQuery(document).bind('keydown', 'alt+0',function (evt){playSound("#audio11"); return false; });
-		jQuery(document).bind('keydown', 'alt+n',function (evt){playSound("#audio12"); return false; });
+		jQuery(document).bind('keydown', 'alt+8',function (evt){playSound("#audio11"); return false; });
+		jQuery(document).bind('keydown', 'alt+3',function (evt){playSound("#audio12"); return false; });
 		
 		jQuery(document).bind('keydown', 'alt+4',function (evt){playSound("#audio13"); return false; });
-		//jQuery(document).bind('keydown', 'alt+g',function (evt){playSound("#audio14"); return false; });
-		//jQuery(document).bind('keydown', 'alt+g',function (evt){playSound("#audio15"); return false; });
-		//jQuery(document).bind('keydown', 'alt+g',function (evt){playSound("#audio16"); return false; });		
+		jQuery(document).bind('keydown', 'alt+z',function (evt){playSound("#audio14"); return false; });
+		jQuery(document).bind('keydown', 'alt+2',function (evt){playSound("#audio15"); return false; });
+		jQuery(document).bind('keydown', 'alt+0',function (evt){playSound("#audio16"); return false; });	
+		jQuery(document).bind('keydown', 'alt+e',function (evt){playSound("#audio17"); return false; });	
+		jQuery(document).bind('keydown', 'alt+m',function (evt){playSound("#audio18"); return false; });	
 		
 	 }
 	 
