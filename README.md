897e7a7e9552052fed9e23626302d701ed7e3126
Eduardo Mioto <eduardo.mioto@ge.com>
2016-11-11 18:30:01 -0200

    Alterações no dinamismo das funções de click e toque. Inclusão de novos toques. Criação de placeholders para inclusão de novos toques.

--- README.md
+++ README.md
@@ -1,23 +1,29 @@
 # butoneira
 ###### Documentação:
 	
--[FILA 1, POSIÇÃO 1] Pelé 	(ALT + P)		
--[FILA 1, POSIÇÃO 2] Hadouken 	(ALT + H)
--[FILA 1, POSIÇÃO 3] Grilo 	(ALT + G)
--[FILA 1, POSIÇÃO 4] Arquivo X	(ALT + X)     	
+- [FILA 1, POSIÇÃO 1] Pelé 		(ALT + P)		
+- [FILA 1, POSIÇÃO 2] Hadouken 	(ALT + H)
+- [FILA 1, POSIÇÃO 3] Grilo		(ALT + G)
+- [FILA 1, POSIÇÃO 4] Arquivo X	(ALT + X)     	
 	
--[FILA 2, POSIÇÃO 1] Chaves Triste  		(ALT + T)		
--[FILA 2, POSIÇÃO 2] Kiko Bola  	  	(ALT + K)		
--[FILA 2, POSIÇÃO 3] Moises To Entendendo   	(ALT + M)		
--[FILA 2, POSIÇÃO 4] Flauta			(ALT + F)
+- [FILA 2, POSIÇÃO 1] Chaves Triste  		(ALT + T)		
+- [FILA 2, POSIÇÃO 2] Kiko Bola  		  	(ALT + K)		
+- [FILA 2, POSIÇÃO 3] Moises To Entendendo 	(ALT + M)		
+- [FILA 2, POSIÇÃO 4] Flauta				(ALT + F)
 	
--[FILA 3, POSIÇÃO 1] Joao Kleber		(ALT + J)
--[FILA 3, POSIÇÃO 2]    	  		(ALT + ??)
--[FILA 3, POSIÇÃO 3]       			(ALT + ??)
--[FILA 3, POSIÇÃO 4] 				(ALT + ??)
-	
-Sugestões:
-	-Mestre dos Magos
-	-Robot
-	-Palmas
-	-
+- [FILA 3, POSIÇÃO 1] Joao Kleber				(ALT + J)
+- [FILA 3, POSIÇÃO 2] Jeremias - Matarra 1000   (ALT + 1)
+- [FILA 3, POSIÇÃO 3] Ta vazii 					(ALT + 0)
+- [FILA 3, POSIÇÃO 4] Hoje Não					(ALT + N)	
+
+- [FILA 4, POSIÇÃO 1] Tetra						(ALT + 4)
+- [FILA 4, POSIÇÃO 2] ?????						(ALT + ?)
+- [FILA 4, POSIÇÃO 3] ????? 					(ALT + ?)
+- [FILA 4, POSIÇÃO 4] ?????						(ALT + ?)
+##### Sugestões:
+ Mestre dos Magos
+- Robot
+- Palmas
+
+##### Demo
+-URL: https://s3-sa-east-1.amazonaws.com/butoneira/home.html
--- custom/butoneira.js
+++ custom/butoneira.js
@@ -1,110 +1,45 @@
 
 	 $(document).ready(function() {
 
-		$( "#btnSound1" ).click(function() {
-			playSound1();
-		});
-		$( "#btnSound2" ).click(function() {
-			playSound2();
-		});
-		$( "#btnSound3" ).click(function() {
-			playSound3();
-		});
-		$( "#btnSound4" ).click(function() {
-			playSound4();
-		});
-		
-		$( "#btnSound5" ).click(function() {
-			playSound5();
-		});
-		$( "#btnSound6" ).click(function() {
-			playSound6();
-		});
-		$( "#btnSound7" ).click(function() {
-			playSound7();
-		});
-		$( "#btnSound8" ).click(function() {
-			playSound8();
-		});	
+		$( 'img[id^="btnSound"]' ).click(function() { 
+			var refId = $(this).attr('id').match(/\d+/)[0];
+			var audioId = "#audio" + refId; 
+			playSound(audioId)
+		;});	
 		
 		sound();
 	 });
 	 
-	 function playSound1(){
-		if($("#audio1")[0].currentTime > 0){
-				$("#audio1")[0].pause();
-				$("#audio1")[0].currentTime = 0.0;
+	 function playSound(audioId){
+    	console.log(audioId)
+		if($(audioId)[0].currentTime > 0){
+			$(audioId)[0].pause();
+			$(audioId)[0].currentTime = 0.0;
 		}else{
-			$("#audio1")[0].play();
-		}
-	 }
-	 
-	  function playSound2(){
-		if($("#audio2")[0].currentTime > 0){
-				$("#audio2")[0].pause();
-				$("#audio2")[0].currentTime = 0.0;
-		}else{
-			$("#audio2")[0].play();
-		}
-	 }
-	 
-	  function playSound3(){
-		if($("#audio3")[0].currentTime > 0){
-				$("#audio3")[0].pause();
-				$("#audio3")[0].currentTime = 0.0;
-		}else{
-			$("#audio3")[0].play();
-		}
-	 }
-	 
-	  function playSound4(){
-		if($("#audio4")[0].currentTime > 0){
-				$("#audio4")[0].pause();
-				$("#audio4")[0].currentTime = 0.0;
-		}else{
-			$("#audio4")[0].play();
-		}
-	 }
-	 
-	  function playSound5(){
-		if($("#playSound5")[0].currentTime > 0){
-				$("#playSound5")[0].pause();
-				$("#playSound5")[0].currentTime = 0.0;
-		}else{
-			$("#playSound5")[0].play();
-		}
-	 }
-	 
-	  function playSound6(){
-		if($("#audio6")[0].currentTime > 0){
-				$("#audio6")[0].pause();
-				$("#audio6")[0].currentTime = 0.0;
-		}else{
-			$("#audio6")[0].play();
-		}
-	 }
-	 
-	  function playSound7(){
-		if($("#audio7")[0].currentTime > 0){
-				$("#audio7")[0].pause();
-				$("#audio7")[0].currentTime = 0.0;
-		}else{
-			$("#audio7")[0].play();
-		}
-	 }
-	 
-	 function playSound8(){
-		if($("#audio6")[0].currentTime > 0){
-				$("#audio8")[0].pause();
-				$("#audio8")[0].currentTime = 0.0;
-		}else{
-			$("#audio8")[0].play();
+			$(audioId)[0].play();
 		}
 	 }
 	 
 	 function sound(){
-		jQuery(document).bind('keydown', 'alt+l',function (evt){playSound1(); return false; });
-		jQuery(document).bind('keydown', 'alt+h',function (evt){playSound2(); return false; });
-		jQuery(document).bind('keydown', 'alt+g',function (evt){playSound3(); return false; });
+		jQuery(document).bind('keydown', 'alt+p',function (evt){playSound("#audio1"); return false; });
+		jQuery(document).bind('keydown', 'alt+h',function (evt){playSound("#audio2"); return false; });
+		jQuery(document).bind('keydown', 'alt+g',function (evt){playSound("#audio3"); return false; });
+		jQuery(document).bind('keydown', 'alt+x',function (evt){playSound("#audio4"); return false; });
+		
+		jQuery(document).bind('keydown', 'alt+t',function (evt){playSound("#audio5"); return false; });
+		jQuery(document).bind('keydown', 'alt+k',function (evt){playSound("#audio6"); return false; });
+		jQuery(document).bind('keydown', 'alt+m',function (evt){playSound("#audio7"); return false; });
+		jQuery(document).bind('keydown', 'alt+f',function (evt){playSound("#audio8"); return false; });
+		
+		jQuery(document).bind('keydown', 'alt+j',function (evt){playSound("#audio9"); return false; });
+		jQuery(document).bind('keydown', 'alt+1',function (evt){playSound("#audio10"); return false; });
+		jQuery(document).bind('keydown', 'alt+0',function (evt){playSound("#audio11"); return false; });
+		jQuery(document).bind('keydown', 'alt+n',function (evt){playSound("#audio12"); return false; });
+		
+		jQuery(document).bind('keydown', 'alt+4',function (evt){playSound("#audio13"); return false; });
+		//jQuery(document).bind('keydown', 'alt+g',function (evt){playSound("#audio14"); return false; });
+		//jQuery(document).bind('keydown', 'alt+g',function (evt){playSound("#audio15"); return false; });
+		//jQuery(document).bind('keydown', 'alt+g',function (evt){playSound("#audio16"); return false; });		
+		
 	 }
 	 
